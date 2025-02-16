9835886eb4cc74d299b1b20f960d1bd907db5313
ЕвгенияПокидина <ovee9191@gmail.com>
2025-02-16 18:57:30 +0200

    Lesson 35.2

---
+++ Lesson 35.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Formats.Asn1;
+using System.Globalization;
+using System;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Linq;
+
+StreamReader reader = new StreamReader("Movies.csv");
+CsvReader csvReader = new CsvReader(reader,
+    new CsvConfiguration(CultureInfo.InvariantCulture)
+    {
+        Delimiter = ",",
+        HasHeaderRecord = true,
+        HeaderValidated = null,
+         BadDataFound = null
+    });
+List<Movie> records = csvReader.GetRecords<Movie>().ToList();
+reader.Close();
+
+var sortedMovies = records.OrderByDescending(f => f.Year).ThenBy(f => f.Name);
+
+foreach (Movie record in sortedMovies)
+{
+    Console.WriteLine("Номер: " + record.Id + " Название: " + record.Name + " Дата: " + record.Year + " Режиссер: " + record.Director + " Жанр: " + record.Genre + " Рейтинг: " + record.Graduate);
+}
+class Movie
+{
+    public string? Id { get; set; }
+    public string? Name { get; set; }
+    public int Year { get; set; }
+    public string? Director { get; set; }
+    public string? Genre { get; set; }
+    public double Graduate { get; set; }
+}
