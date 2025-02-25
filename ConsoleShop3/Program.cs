ade66eea52dac224a6182ffe796f2c2cc930ddcd
ЕвгенияПокидина <ovee9191@gmail.com>
2025-02-25 18:06:35 +0200

    Lesson 39

---
+++ ConsoleShop3/Program.cs
@@ -0,0 +1,126 @@
+﻿using System.Numerics;
+using System.Reflection.Metadata;
+using System;
+using System.Collections.Generic;
+
+Random random = new Random();
+Console.Write("Введите имя: ");
+string name = Console.ReadLine()!;
+Console.Write("Введите возраст: ");
+int age = int.Parse(Console.ReadLine()!);
+Console.Write("Введите баланс: ");
+decimal balance = decimal.Parse(Console.ReadLine()!);
+Console.Write("Введите число от 1 до 3: ");
+int number = int.Parse(Console.ReadLine()!);
+int randomNumber = random.Next(1, 4);
+IUser user = new User(name, balance, age);
+
+if (randomNumber == number)
+{
+    Console.WriteLine("Вы счастливый покупатель, ваш баланс удвоился");
+    user = new HappyUser(name, balance, age);
+}
+
+
+CPU cpu1=new CPU("Intel Core i7", 27000, "China", 3200);
+CPU cpu2 = new CPU("AMD", 25000, "China", 3000);
+CoolingSystem coolingSystem= new CoolingSystem("Depcool", 600, "China", 100);
+CoolingSystem coolingSystem1 = new CoolingSystem("Cooler master", 890, "China", 100);
+List<Product> products = new List<Product>() {cpu1, cpu2, coolingSystem, coolingSystem1 };
+public class Product
+{
+    public string? Name { get; set; }
+    public decimal? Price { get; set; }
+    public string? Manufacturer { get; set; }
+    public Product(string? name, decimal? price, string? manufacturer)
+    {
+        Name = name;
+        Price = price;
+        Manufacturer = manufacturer;
+    }
+    public virtual decimal? GetDiscount(IUser user, DateOnly dateSale)
+    {
+        if (user.Spend > 50000) Price *= 0.9M;
+        else if (user.Spend > 10000) Price *= 0.95M;
+        Dictionary<int, int> calendar = new Dictionary<int, int>()
+    {
+    { 1,7},
+    { 2,14},
+    { 3,8},
+    { 4,15},
+    { 5,9},
+    { 6,1},
+    { 7,12},
+    { 8,9},
+    { 9,1},
+    { 10,12},
+    { 11,21},
+    { 12,31}
+    };
+        foreach (var cal in calendar)
+        {
+            if (cal.Value == dateSale.Day && cal.Key == dateSale.Month) Price *= 0.8M;
+        }
+        return Price;
+    }
+}
+public interface IUser
+{ 
+    public string Name { get; set; }
+    public decimal? Balance { get; set; }
+    public decimal? Spend { get; set; }
+    public int? Age { get; set; }
+    public void ReduceBalace(decimal price, int quantity);
+}
+public class User : IUser
+{
+    public string Name { get; set; }
+    public decimal? Balance { get; set; }
+    public decimal? Spend { get; set; }
+    public int? Age { get; set; }
+    public User(string name, decimal? balance, int? age)
+    {
+        Name = name;
+        Balance = balance;
+        Age = age;
+        Spend = 0;
+    }
+
+    public void ReduceBalace(decimal price, int quantity)
+    {
+        decimal total = price * quantity;
+        Spend += total;
+        Balance -= total;
+    }
+}
+public class CPU : Product
+{
+    public double Frequency { get; set; }
+    public CPU(string? name, decimal? price, string? manufacturer, double frequency) : base(name, price, manufacturer)
+    {
+        Frequency = frequency;
+    }
+}
+public class CoolingSystem : Product
+{
+    public double TDP { get; set; }
+    public CoolingSystem(string? name, decimal? price, string? manufacturer, double tDP) : base(name, price, manufacturer)
+    {
+        TDP = tDP;
+    }
+    public override decimal? GetDiscount(IUser user, DateOnly dateSale)
+    {
+        if (user.Age > 30)
+        {
+            Price *= 0.75m;
+        }
+        return Price;
+    }
+}
+public class HappyUser : User
+{
+    public HappyUser(string name, decimal? balance, int? age) : base(name, balance, age)
+    {
+        Balance = balance * 3;
+    }
+}
---
+++ Lesson 39/Program.cs
@@ -0,0 +1,169 @@
+﻿using System.ComponentModel.Design;
+using System.Security.Authentication;
+List<Person> persons = new List<Person>()
+{
+    new Person(){Id=1, Name="Иван",Age=35},
+    new Person(){Id=2,Name="Борис",Age=24},
+    new Person(){Id=3,Name="Федор",Age=59}
+};
+List<Product> priceList = new List<Product>
+{
+    new Product(){Name="Asus",ProductType=TypeOfProduct.MotherBoard,Price=15000,Id=1},
+    new Product(){Name="Intel",ProductType=TypeOfProduct.Cpu,Price=9000,Id=2},
+    new Product(){Name="NVidia",ProductType=TypeOfProduct.VideoCard,Price=12000,Id=3},
+    new Product(){Name="GeForce",ProductType=TypeOfProduct.MotherBoard,Price=17500,Id=4},
+    new Product(){Name="AMD",ProductType=TypeOfProduct.Cpu,Price=11000,Id=5},
+    new Product(){Name="MegaByte",ProductType=TypeOfProduct.VideoCard,Price=14000,Id=6},
+    new Product(){Name="MSI",ProductType=TypeOfProduct.PowerUnit,Price=500,Id=7,Discount=0.5M},
+    new Product(){Name="Tower",ProductType=TypeOfProduct.Frame,Price=800,Id=8},
+    new Product(){Name="Water",ProductType=TypeOfProduct.CoolingSystem,Price=200,Id=9}
+};
+Console.Write("Введите ваше имя:");
+string name = Console.ReadLine()!;
+Console.Write("Введите ваш баланс:");
+decimal balance = decimal.Parse(Console.ReadLine()!);
+Person person = persons.FirstOrDefault(p => p.Name == name)!;
+decimal discount = 0;
+DateOnly dateSale = new DateOnly();
+Console.Write("Введите дату продажи в формате YYYY.MM.dd:");
+dateSale = DateOnly.Parse(Console.ReadLine()!);
+Dictionary<int, int> calendar = new Dictionary<int, int>()
+{
+        {1,7},
+        {2,14},
+        {3,8},
+        {4,15},
+        {5,9},
+        {6,1},
+        {7,12},
+        {8,9},
+        {9,1},
+        {10,12},
+        {11,21},
+        {12,31}
+};
+if (person == null)
+    Console.WriteLine("Такого пользователя нет!");
+else
+{
+
+    person.Balance = balance;
+    decimal Startbalance = balance;
+    SalePerson salePerson = new SalePerson();
+    salePerson.Customer = person;
+    salePerson.Sales = new List<Sale>();
+    do
+    {
+        Console.Clear();
+        Console.WriteLine("Ваше имя:" + person.Name);
+        Console.WriteLine("Ваш возраст:" + person.Age);
+        Console.WriteLine("Сегодня:" + dateSale);
+        Console.WriteLine($"Ваш баланс:{person.Balance}");
+        Console.WriteLine("Список товаров:");
+        foreach (var item in priceList)
+        {
+            foreach (var cal in calendar)
+            {
+                discount = (cal.Value == dateSale.Day && cal.Key == dateSale.Month) ? 0.2M : 0;
+            }
+            if (Startbalance - person.Balance > 50000)
+            {
+                discount = discount + 0.1M;
+            }
+            else if (Startbalance - person.Balance > 10000 && Startbalance - person.Balance <= 50000)
+            {
+                discount = discount + 0.05M;
+            }
+
+            if (item.Discount == 0)
+                Console.WriteLine($"Id:{item.Id}. Name:{item.Name} Type:{item.ProductType}" +
+                $" Price:{item.Price - item.Price * discount}");
+            else
+            {
+                if (item.Name == "Water" && person.Age > 30)
+                {
+                    Console.WriteLine($"Id:{item.Id}. Name:{item.Name} Type:{item.ProductType}" +
+                   $" Price:{item.Price - item.Price * (discount + 0.25M)}");
+                }
+                else if (calendar.ContainsValue(dateSale.Day) && calendar.ContainsKey(dateSale.Month) == false)
+                    Console.WriteLine($"Id:{item.Id}. Name:{item.Name} Type:{item.ProductType}" +
+                    $" Price:{item.Price - item.Price * (item.Discount)}");
+                else Console.WriteLine($"Id:{item.Id}. Name:{item.Name} Type:{item.ProductType}" +
+                      $" Price:{item.Price - item.Price * (item.Discount + 0.2M)}");
+
+            }
+        }
+        Console.WriteLine("Список покупок:");
+        foreach (var item in salePerson.Sales)
+        {
+            Console.WriteLine(item.Product.Name + " " + item.Count + " " + item.Total());
+        }
+        if (person.Balance > 0)
+        {
+            Console.Write("Введите номер товара:");
+            int id = int.Parse(Console.ReadLine()!);
+            Console.Write("Введите его количество:");
+            int count = int.Parse(Console.ReadLine()!);
+            Sale sale = new Sale();
+            sale.Product = priceList.First(p => p.Id == id);
+            sale.Count = count;
+            if (person.Balance >= sale.Total())
+            {
+                if (sale.Product.Discount == 0)
+                    sale.Discount = discount;
+                else
+                    sale.Discount = sale.Product.Discount;
+                salePerson.Sales.Add(sale);
+                person.Balance -= sale.Total();
+            }
+            else
+            {
+                Console.WriteLine("Не хватает средств для покупки!");
+                Console.ReadKey();
+            }
+        }
+        else
+        {
+            Console.WriteLine("На счету нет денег!");
+            Console.ReadKey();
+        }
+    }
+    while (true);
+}
+enum TypeOfProduct
+{
+    MotherBoard,
+    Cpu,
+    VideoCard,
+    PowerUnit,
+    Frame,
+    CoolingSystem
+}
+class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public TypeOfProduct ProductType { get; set; }
+    public decimal Price { get; set; }
+    public decimal Discount { get; set; }
+}
+class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Balance { get; set; }
+    public int Age { get; set; }
+}
+class Sale
+{
+    public Product Product { get; set; }
+    public int Count { get; set; }
+    public decimal Discount { get; set; }
+    public decimal Total() => Count * Product.Price - Count * Product.Price * Discount;
+}
+class SalePerson
+{
+    public Person Customer { get; set; }
+    public List<Sale> Sales { get; set; }
+    public decimal GetTotal(int count) => Sales.Sum(p => p.Total());
+}
